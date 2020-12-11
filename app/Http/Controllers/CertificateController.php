e067c4a3e7dbe091bbeb7774bd5256e091f89c63
Ahmed-Elhadary <ahmedhamamibrahim@gmail.com>
2020-12-12 00:11:41 +0200

    Add Certificate Controller

---
+++ app/Http/Controllers/CertificateController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CertificateRequest;
+use App\Models\Certificate;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CertificateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('certificate.show',["certificates"=>Certificate::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+     return  view('certificate.create')->with('users',User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CertificateRequest $request)
+    {
+        Certificate::create($request->all());
+
+        return redirect(route('certificates.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Certificate  $certificate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Certificate $certificate)
+    {
+        return view("certificate.showsingle", compact("certificate"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Certificate  $certificate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Certificate $certificate)
+    {
+        return view('certificate.edit')->with('certificates',$certificate)->with('users',User::all());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Certificate  $certificate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CertificateRequest $request, Certificate $certificate)
+    {
+        $certificate->update($request->all());
+      return redirect()->route('certificates.index')->with('certificates',$certificate)->with('users',User::all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Certificate  $certificate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Certificate $certificate)
+    {
+        //
+        $certificate->delete();
+        return back();
+    }
+}
