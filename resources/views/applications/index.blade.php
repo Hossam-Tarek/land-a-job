ef559160c31a731519666472e4edfdbe6aaa985b
Marwa Gado <mgado148@gmail.com>
2020-12-12 16:20:26 +0200

    Edit view index

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -1,36 +1,39 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="container">
-        <div class="row">
-            <div>
-                <h1>All applications</h1>
-                <a href="{{ route("applications.create") }}" class="btn btn-primary">Add Application</a>
-                <table class="table table-striped">
-                    <thead>
+<a href="{{route('applications.create')}}" class="btn btn-success">Add new Application</a>
+        @if($applications->count()>0)
+            <h1 class="text-center mb-3">All Applications</h1>
+            <table class="table table-striped">
+                <thead class="bg-secondary text-light">
                     <tr>
-                        <th>jobId</th>
-                        <th>status</th>
-                        <th></th>
+                        <td>jobId</td>
+                        <td>status</td>
+                        <td class="text-right">Actions</td>
                     </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($applications as $application)
-                            <tr>
-                                <td>{{ $application->job_id}}</td>
-                                <td>{{ $application->status }}</td>
-                                <td>
-                                    <form action="{{ route("applications.destroy", $application) }}" method="POST">
-                                        @csrf
-                                        @method("DELETE")
-                                        <input type="submit" class="btn btn-danger" value="Delete">
-                                    </form>
+                </thead>
+                <tbody>
+                    @foreach($applications as $application)
+                        <tr>
+                            <td>{{ $application->job_id}}</td>
+                            <td>{{ $application->status }}</td>
+                            <td class="text-right">
+                                <form action="{{ route("applications.destroy", $application) }}" method="POST">
+                                    @csrf
+                                    @method("DELETE")
+                                    <input type="submit" class="btn btn-danger" value="Delete">
+                                </form>
                                 </td>
                             </tr>
-                        @endforeach
+                    @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+    @else
+        <div class="card-header text-center">
+            <h2>no Applications yet</h2>
+        </div>
+    @endif
 @endsection
