fd3c27a2044a1dda70a8f430bbcb50b3b71f8cad
Hossam Tarek <hossamtarek250@gmail.com>
2020-12-13 09:50:05 +0200

    Add footer

---
+++ resources/views/company/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark">
+    Copyright &copy; 2020, Land a job All rights reserved
+</footer>
--- resources/views/company/layouts/master.blade.php
+++ resources/views/company/layouts/master.blade.php
@@ -17,6 +17,8 @@
         @yield("content")
     </div>
 
+    @include("company.includes.footer")
+
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
     <script src="{{ asset("js/company/main.js") }}"></script>
