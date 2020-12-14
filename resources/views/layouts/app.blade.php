f38326109c019d2b4a61a78da69dad006edd2c57
Khalid-Abed <khalidabeed24@gmail.com>
2020-12-14 21:05:51 +0200

    comment on old layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     @yield('css')
 </head>
 <body>
