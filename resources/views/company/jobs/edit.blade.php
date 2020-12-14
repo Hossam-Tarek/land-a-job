c47a71b4ac31e1532b6360d5232a6a12ee924614
Khalid-Abed <khalidabeed24@gmail.com>
2020-12-14 20:44:48 +0200

    Add views of job for company view

---
+++ resources/views/company/jobs/edit.blade.php
@@ -0,0 +1,290 @@
+@extends("company.layouts.master")
+@section("title", "Create New Job")
+@section('style-sheets')
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+@endsection
+@section('content')
+ <div class="card">
+        <div class="card-header bg-secondary text-light">
+        <h4>Update {{$job->title}}</h4>
+        </div>
+
+        <div class="card-body">
+            <form action="{{route('all-jobs.update',$job->id)}}" method='post' enctype='multipart/form-data'>
+                @csrf
+                @method('put')
+
+                <div class="form-group">
+                    <label class="col-form-label-lg" for="title">Job Title</label>
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                         <span class="glyphicon glyphicon-subtitles"></span>
+                        </div>
+                        <input type="text" name='title' class="form-control @error('title') error @enderror" value="{{$job->title}}">
+                    </div>
+                    @error('title')
+                    <li class="text-error">{{$message}}</li>
+                    @enderror
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="Job Type"> Job Type</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-folder-open"></span>
+                            </div>
+                            <select name="job_type_id"  class="form-control @error('job_type_id') error @enderror">
+                                <option></option>
+                                @foreach($jobTypes as $jobType)
+                                    <option value="{{$jobType->id}}"
+                                    @if($jobType->id === $job->job_type_id) selected @endif
+                                        > {{$jobType->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('job_type_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                       </div>
+
+                       <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="industry_category_id"> Industry Category</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-modal-window"></span>
+                            </div>
+                            <select name="industry_category_id"  class="form-control @error('industry_category_id') error @enderror" value="{{old('industry_category_id')}}">
+                                <option></option>
+                                @foreach($industryCategories as $industryCategory)
+                                    <option value="{{$industryCategory->id}}"
+                                    @if($industryCategory->id === $job->industry_category_id) selected @endif
+                                        > {{$industryCategory->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('industry_category_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="career_level_id"> Career Level</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-credit-card"></span>
+                            </div>
+                        <select name="career_level_id"  class="form-control @error('career_level_id') error @enderror" value="{{old('career_level_id')}}">
+                            <option></option>
+                            @foreach($careerLevels as $careerLevel)
+                                <option value="{{$careerLevel->id}}"
+                                    @if($careerLevel->id === $job->career_level_id) selected @endif
+                                    > {{$careerLevel->name}}</option>
+                            @endforeach
+                        </select>
+                        </div>
+                        @error('career_level_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="company_id"> Company</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-compressed"></span>
+                            </div>
+                        <select name="company_id"  class="form-control @error('company_id') error @enderror" value="{{old('company_id')}}">
+                            <option></option>
+                            @foreach($companies as $company)
+                                <option value="{{$company->id}}"
+                                @if($company->id === $job->company_id) selected @endif
+                                    > {{$company->name}}</option>
+                            @endforeach
+                        </select>
+                      </div>
+                        @error('company_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="country_id"> Country</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-flag"></span>
+                            </div>
+                        <select name="country_id"  class="form-control @error('country_id') error @enderror" value="{{old('country_id')}}">
+                            <option></option>
+                            @foreach($countries as $country)
+                                <option value="{{$country->id}}"
+                                    @if($country->id === $job->country_id) selected @endif
+                                    > {{$country->name}}</option>
+                            @endforeach
+                        </select>
+                        </div>
+                        @error('country_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="city_id"> City</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-home"></span>
+                            </div>
+                        <select name="city_id"  class="form-control @error('city_id') error @enderror" value="{{old('city_id')}}">
+                            <option></option>
+                            @foreach($cities as $city)
+                                <option value="{{$city->id}}"
+                                    @if($city->id === $job->city_id) selected @endif
+                                    > {{$city->name}}</option>
+                            @endforeach
+                        </select>
+                        </div>
+                        @error('city_id')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="status">Status</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-envelope"></span>
+                            </div>
+                        <select name="status" class="form-control @error('status') error @enderror" value="{{$job->status}}">
+                            <option></option>
+                            <option value="1" @if($job->status === 1) selected @endif>True</option>
+                            <option value="0" @if($job->status === 0) selected @endif>False</option>
+                        </select>
+                        </div>
+                        @error('status')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="vacancies">Vacancies</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-user"></span>
+                            </div>
+                        <input type="text" name="vacancies" class="form-control @error('vacancies') error @enderror" value="{{$job->vacancies}}">
+                        </div>
+                        @error('vacancies')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="min_years_of_experience">Minimum Year Of Experience</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-cloud-download"></span>
+                            </div>
+                        <input type="text" name="min_years_of_experience" class="form-control @error('min_years_of_experience') error @enderror" value="{{$job->min_years_of_experience}}">
+                        </div>
+                        @error('min_years_of_experience')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="max_years_of_experience">Max Year Of Experience</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-cloud-upload"></span>
+                            </div>
+                        <input type="text" name="max_years_of_experience" class="form-control @error('max_years_of_experience') error @enderror" value="{{$job->max_years_of_experience}}">
+                        </div>
+                        @error('max_years_of_experience')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="min_salary">Minimum Salary</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                             <span class="glyphicon glyphicon-eur"></span>
+                            </div>
+                        <input type="text" name="min_salary" class="form-control @error('min_salary') error @enderror" value="{{$job->min_salary}}">
+                        </div>
+                        @error('min_salary')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label class="col-form-label-lg" for="max_salary">Maximum Salary</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-yen"></span>
+                            </div>
+                        <input type="text" name="max_salary" class="form-control @error('max_salary') error @enderror" value="{{$job->max_salary}}">
+                      </div>
+                        @error('max_salary')
+                        <li class="text-error">{{$message}}</li>
+                        @enderror
+                    </div>
+                </div>
+
+
+
+                <div class="form-group">
+                    <label class="form-label-lg" for="skill">Select Skills</label><br>
+                       <select name="skills[]" id="" multiple class="form-control">
+                            @foreach($skills as $skill)
+                                <option value="{{$skill->id}}"
+                                    @foreach ($job->skills as $jobskill)
+                                        @if ($skill->id == $jobskill->id)
+                                            selected
+                                        @endif
+                                    @endforeach
+                                    >{{$skill->name}}</option>
+                            @endforeach
+                       </select>
+                </div>
+
+
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control @error('description') error @enderror" name="description" id="content" cols="5" rows="5">{{$job->description}}</textarea>
+                    @error('description')
+                    <li class="text-error">{{$message}}</li>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="requirements">Requirements</label>
+                    <textarea class="form-control @error('requirements') error @enderror" name="requirements" id="content" cols="5" rows="5">{{$job->requirements}}</textarea>
+                    @error('requirements')
+                    <li class="text-error">{{$message}}</li>
+                    @enderror
+                </div>
+
+                <button class="btn btn-success" type='submit'>Update Job</button>
+                <a class="btn btn-primary" href="{{route('all-jobs.index')}}">Cancel</a>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/company/jobs/show.blade.php
@@ -0,0 +1,117 @@
+@extends("company.layouts.master")
+
+@section("title", "Single Jobs")
+
+@section('style-sheets')
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+@endsection
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+             <div class="col-sm-8" > {{--Parent Div => Left Div --}}
+
+                <div class="row p-5 bg-white bord">
+                    <div class="col-8 ">
+                        <h3>{{$job->title}}</h3>
+                        <h3 class="text-primary">{{$job->company->name}}</h3>
+                        <h6>{{$job->country->name}}</h6>
+                        <a href="{{route('all-jobs.create')}}" class="btn btn-success my-5 w-75 p-3 ">Post a job Like This</a>
+                    </div>
+                    <div class="col-4">
+                        <img src="{{asset('avatar/'.$job->company->logo)}}" alt="Not Yet " class="logo mb-4">
+                        <h5 class="my-5">{{$job->applications->count()}} Applicants for 1 open Position</h5>
+                    </div>
+                </div>
+
+                <div class="row p-5 bg-white my-5 bord">
+                   <div class="container">
+                        <div class="row text-center">
+                            <div class="col-lg-4 col-sm-12">
+                                <h4>Experience Needed</h4>
+                                <span> {{$job->min_years_of_experience}} to {{$job->max_years_of_experience}}  years</span>
+                            </div>
+                            <div class="col-lg-4 col-sm-12">
+                                <h4>Career Level:</h4>
+                                <span>{{$job->careerLevel->name}}</span>
+                            </div>
+                            <div class="col-lg-4 col-sm-12">
+                                <h4>Job Type:</h4>
+                                <span>{{$job->jobType->name}}</span>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row text-center">
+                            <div class="col-lg-4">
+                                <h4>Salary</h4>
+                                <span>{{$job->max_salary}}</span>
+                            </div>
+                            <div class="col-lg-4">
+                                <h4>Vacancies</h4>
+                                <span>{{$job->vacancies}}</span>
+                            </div>
+                        </div>
+                   </div>
+                </div>
+
+                <div class="row p-5 bg-white my-5 bord">
+                    <div class="">
+                        <h3 class="underline">Job Categories:</h3>
+                        <h5>{{$job->industryCategory->name}}</h5>
+                    </div>
+                </div>
+
+                <div class="row p-5 bg-white my-5 bord">
+                    <div class="">
+                        <h3 class="underline">About The Job</h3>
+                        <ul>
+                            @foreach ($descriptions as $item)
+                                @if($item != '')
+                                    <Li>{{$item}}</Li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row p-5 bg-white my-5 bord">
+                    <div class="">
+                        <h3 class="underline">Job Requirements</h3>
+                        <p class="text-secondary"> {{$job->requirements}}</p>
+                    </div>
+                </div>
+
+                <div class="row p-5 bg-white my-5 bord">
+                    <div>
+                        <h3 class="mb-5 underline">Key Words</h3>
+                        @foreach ($job->skills as $skill)
+                            <a class="btn btn-primary m-2 ">{{$skill->name}}</a>
+                        @endforeach
+                    </div>
+                </div>
+
+             </div>
+
+
+            <div class="col-lg-4 col-sm-12">{{--Parent Div => Right Div --}}
+                <div class="p-5 bg-white bord">
+                    <h3 class="underline">About {{$job->company->name}}</h3>
+                    <p class="">{{$job->company->about}}</p>
+                </div>
+
+                <div class="bg-white p-5 my-5 bord">
+                    <h3 class="my-5"> Similar Jobs</h3>
+                    @foreach ($related as $item)
+                        <h3 class="text-secondary underline">{{$item->title}}</h3>
+                        <a href="{{route('company.profile')}}" class="text-primary">{{$item->company->name}}</a>
+                        <h6 >{{$item->city->name}}</h6>
+                        <hr>
+                    @endforeach
+               </div>
+
+            </div>
+
+        </div>
+    </div>
+@endsection
