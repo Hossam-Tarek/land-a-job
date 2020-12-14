134bdfa16ac399350fa97ffac8639655fa333780
Ahmed-Elhadary <ahmedhamamibrahim@gmail.com>
2020-12-14 18:04:43 +0200

    Add Authentication Functions

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -31,10 +35,46 @@ class LoginController extends Controller
     /**
      * Create a new controller instance.
      *
+     *
      * @return void
      */
+
+
+    public function login(Request $request)
+    {
+//        dd($request->all());
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            $user = User::where('email', $request->email)->first();
+            if ($user->is_company()) {
+                return redirect()->route('users.create');
+            } elseif ($user->is_user()) {
+                return redirect()->route('phones.create');
+            } elseif ($user->is_admin()) {
+                return redirect()->route('links.create');
+            }
+        }
+    }
+
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function redirectToProvider($website)
+    {
+        return Socialite::driver($website)->redirect();
+    }
+
+    public function handleProviderCallback($website)
+    {
+        $user=Socialite::driver($website)->stateless()->user();
+
+        return $user->getName();
     }
 }
