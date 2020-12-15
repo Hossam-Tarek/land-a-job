4d449f2c5047097c25984ed18aa718861e6388a3
Khalid-Abed <khalidabeed24@gmail.com>
2020-12-15 14:44:19 +0200

    add class to datatable to be responsive

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,50 +6,51 @@
     @endsection
 
     @section('content')
-    <div class="container">
+
         @if(session()->has('success'))
         <div class="alert alert-success my-5">
             {{session()->get('success')}}
         </div>
         @endif
         <h1 class="text-center text-secondary mt-4">All Users</h1>
-        <table id="table1" class="table table-bordered text-center table-hover">
-            <thead class="bg-secondary">
-                <tr>
-                    <th class="text-center text-white">First Name</th>
-                    <th class="text-center text-white">Last Name</th>
-                    <th class="text-center text-white">Avatar </th>
-                    <th class="text-center text-white">Role</th>
-                    <th class="text-center text-white">Email</th>
-                    <th class="text-center text-white">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                <tr>
-                    <td>{{$user->first_name}}</td>
-                    <td>{{$user->last_name}}</td>
-                    <td><img src="{{asset('avatar/'.$user->image)}}" alt="" class="user-image"></td>
-                    <td> {{$user->role}}</td>
-                    <td>{{$user->email}}</td>
-                    <td>
 
-                            @if($user->role != 'admin')
-                                <form action="{{route('all-users.destroy',$user->id)}}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
-                            @endif
+    <div class="data-table-responsiv ">
+        <div class="container my-5">
+            <table id="table1" class="table table-bordered text-center table-hover">
+                <thead class="bg-secondary">
+                    <tr>
+                        <th class="text-center text-white">First Name</th>
+                        <th class="text-center text-white">Last Name</th>
+                        <th class="text-center text-white">Avatar </th>
+                        <th class="text-center text-white">Role</th>
+                        <th class="text-center text-white">Email</th>
+                        <th class="text-center text-white">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td>{{$user->first_name}}</td>
+                        <td>{{$user->last_name}}</td>
+                        <td><img src="{{asset('avatar/'.$user->image)}}" alt="" class="user-image"></td>
+                        <td> {{$user->role}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>
 
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                                @if($user->role != 'admin')
+                                    <form action="{{route('all-users.destroy',$user->id)}}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger">Delete</button>
+                                    </form>
+                                @endif
 
-    <div class="container my-5">
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
     @endsection
