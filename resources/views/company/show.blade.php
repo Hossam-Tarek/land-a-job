e156245b3f9a640535f8244efdedb3abe8151556
Ahmed Fawzy <36691001+ahmed-fawzy-metwally@users.noreply.github.com>
2020-12-15 15:02:22 +0200

    Add company profile and edit (#20)

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -2,29 +2,123 @@
 
 @section("title", "Company profile")
 
+@section("style-sheets")
+<link rel="stylesheet" href="{{ url('/css/request_loading.css') }}">
+<link href="{{ asset('css/image-upload-with-preview.css') }}" rel="stylesheet">
+@endsection
+
 @section("content")
-    <div class="row">
-        <div class="col-12">
-            <h1 class="mb-4">Company</h1>
-
-            <h3>Name: {{ $company->name }}</h3>
-            <p>Manager: {{ $company->user->first_name." ".$company->user->last_name }}</p>
-            <p>Industry category: {{ $company->industryCategory->name }}</p>
-            <p>Number of
-               employees: {{ "From ".$company->numberOfEmployee->min." to ".$company->numberOfEmployee->max }}</p>
-            <p>Founded date: {{ $company->founded_date }}</p>
-            <p>Location: {{ $company->city->name.", ".$company->country->name }}</p>
-            <p>Website: <a href="{{ $company->url }}">{{ $company->url }}</a></p>
-            <p>About: {{ $company->about }}</p>
-            <div class="mb-3">
-                <img src="{{ asset("avatar/".$company->logo) }}" alt="company logo">
+<div class="row w-75 mx-auto mt-3">
+    <!-- Images Section -->
+    <div id="image-uploaded-message" class="alert" role="alert" style="z-index: 1000000 !important; display: none; position: fixed; top: 50px;left:12.5%;z-index: 150;width: 75%;"></div>
+    <div class="col-12">
+        <div class="row align-items-center">
+            <div class="col-md-4 col-12 avatar-upload mb-md-0 mb-4">
+                <!-- logo Image -->
+                <div class="avatar-edit">
+                    <form action="" method="post" id="upload-logo-form" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <input type='file' id="logoUpload" name="logo">
+                        <label for="logoUpload"></label>
+                    </form>
+                </div>
+                <div class="avatar-preview">
+                    <div id="logoPreview" style='background-image: url({{asset("avatar/$company->logo")}})'>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <img src="{{ asset("avatar/".$company->cover_image) }}" alt="company cover image">
+            <!-- Cover Image -->
+            <div class="col-md-8 col-12 cover-image-upload mb-md-0 mb-4">
+                <div class="cover-image-edit">
+                    <form action="" method="post" id="upload-cover-image-form" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <input type='file' id="coverImageUpload" name="coverImage">
+                        <label for="coverImageUpload"></label>
+                    </form>
+                </div>
+                <div class="cover-image-preview">
+                    <div id="coverImagePreview" style='background-image: url({{asset("avatar/$company->cover_image")}})'>
+                    </div>
+                </div>
             </div>
-
-            <a href="{{ route("companies.edit", $company) }}"
-               class="btn btn-primary">Edit industry category</a>
         </div>
     </div>
+
+    <div class="col-12 mt-5">
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th scope="col">Company</th>
+                    <td scope="row">{{ $company->name }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Manager</th>
+                    <td scope="row">{{ $company->user->first_name." ".$company->user->last_name }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Industry category</th>
+                    <td scope="row">{{ $company->industryCategory->name }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Number of employees</th>
+                    <td scope="row">{{ "From ".$company->numberOfEmployee->min." to ".$company->numberOfEmployee->max }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Founded date</th>
+                    <td scope="row">{{ $company->founded_date }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Location</th>
+                    <td scope="row">{{ $company->city->name.", ".$company->country->name }}</td>
+                </tr>
+                <tr>
+                    <th scope="col">Website</th>
+                    <td scope="row"><a href="{{ $company->url }}">{{ $company->url }}</a></td>
+                </tr>
+                <tr>
+                    <th scope="col">About</th>
+                    <td scope="row">{{ $company->about }}</td>
+                </tr>
+                <tr>
+                    <th colspan="2" class="text-center text-uppercase text-dark">
+                        <h5 class="my-0">Links</h5>
+                    </th>
+                </tr>
+                @forelse($links as $link)
+                <tr>
+                    <th scope="col">{{$link->name}}</th>
+                    <td scope="row">{{$link->url}}</td>
+                </tr>
+                @empty
+                <tr>
+                    <th colspan="2" class="text-center text-danger">There is no links</th>
+                </tr>
+                @endforelse
+
+                <tr>
+                    <th colspan="2" class="text-center text-uppercase text-dark">
+                        <h5 class="my-0">Phone numbers</h5>
+                    </th>
+                </tr>
+                @forelse($phoneNumbers as $index => $phoneNumber)
+                <tr>
+                    <th scope="col">Phone{{$index+1}}</th>
+                    <td scope="row">{{$phoneNumber->number}}</td>
+                </tr>
+                @empty
+                <tr>
+                    <th colspan="2" class="text-center text-danger">There is no phone numbers</th>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    <div class="col-12 text-center">
+        <a href='{{ route("company.edit") }}' class="btn btn-warning w-25">Edit Profile</a>
+    </div>
+</div>
+@endsection
+
+@section("scripts")
+<script src="{{ asset('js/image-upload-with-preview.js') }}"></script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
@@ -27,7 +27,7 @@ use App\Http\Controllers\Company\CompanyController;
 */
 
 Route::get('/', function () {
-   return view('welcome');
+    return view('welcome');
 });
 
 
@@ -40,44 +40,34 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('users',UserController::class);
-Route::resource('/certificates',CertificateController::class);
-Route::resource('job-titles',JobtitleController::class);
-Route::resource('links',LinkController::class);
-Route::resource('phones',PhoneNumberController::class);
-Route::get('/company/register',function (){
+Route::resource('users', UserController::class);
+Route::resource('/certificates', CertificateController::class);
+Route::resource('job-titles', JobtitleController::class);
+Route::resource('links', LinkController::class);
+Route::resource('phones', PhoneNumberController::class);
+Route::get('/company/register', function () {
     return view('auth.company-register');
 })->name('company-register');
-Route::get('/login',function (){
+Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
-Route::get('/register',function (){
+Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
 
-Route::prefix('admin')->group(function(){
-    Route::resource('jobs',JobController::class);
-    Route::resource('skills',SkillController::class);
-    Route::resource('countries',CountryController::class);
-    Route::resource('jobTypes',JobTypeController::class);
-    Route::resource('careerLevels',CareerLevelController::class);
-    Route::view('/','admin.index');
+Route::prefix('admin')->group(function () {
+    Route::resource('jobs', JobController::class);
+    Route::resource('skills', SkillController::class);
+    Route::resource('countries', CountryController::class);
+    Route::resource('jobTypes', JobTypeController::class);
+    Route::resource('careerLevels', CareerLevelController::class);
+    Route::view('/', 'admin.index');
 
+    Route::get('all-users', [UserController::class, 'allUsers'])->name('all-users.index');
+    Route::delete('delete-user/{id}', [UserController::class, 'destroyUser'])->name('all-users.destroy');
 
-
-    //Khaled
-    Route::get('all-users',[UserController::class,'allUsers'])->name('all-users.index');
-    Route::delete('delete-user/{id}',[UserController::class,'destroyUser'])->name('all-users.destroy');
-
-    Route::get('all-companies',[\App\Http\Controllers\CompanyController::class,'allCompanies'])->name('all-companies.index');
-    Route::delete('delete-company/{id}',[\App\Http\Controllers\CompanyController::class,'destroyCompany'])->name('all-companies.destroy');
-
-
-});
-
-Route::prefix('company')->group(function(){
-    Route::put('updateStatus/{id}' , [ApplicationController::class, 'updateStatus'])->name("application.updatestatus");
-    Route::get('jobApplications/{id}' , [JobController::class, 'getJobApplications'])->name("job.jobApplications");
+    Route::get('all-companies', [\App\Http\Controllers\CompanyController::class, 'allCompanies'])->name('all-companies.index');
+    Route::delete('delete-company/{id}', [\App\Http\Controllers\CompanyController::class, 'destroyCompany'])->name('all-companies.destroy');
 });
 
 Route::resource("/companies", \App\Http\Controllers\CompanyController::class);
@@ -92,25 +82,49 @@ Route::resource("/number-of-employees", \App\Http\Controllers\NumberOfEmployeeCo
 Route::prefix("company")->group(function () {
     Route::get("/", [\App\Http\Controllers\Company\CompanyController::class, "index"])
         ->name("company");
+
     Route::get("/profile", [\App\Http\Controllers\Company\CompanyController::class, "show"])
         ->name("company.profile");
-    Route::get('all-Jobs',[CompanyController::class,'allJobs'])
+
+    Route::get("/edit", [\App\Http\Controllers\Company\CompanyController::class, "edit"])
+        ->name("company.edit");
+    Route::put("/update/{company}", [\App\Http\Controllers\Company\CompanyController::class, "update"])
+        ->name("company.update");
+
+    Route::get("/register", [\App\Http\Controllers\Company\CompanyController::class, "create"])
+        ->name("company.create");
+    Route::post("/store", [\App\Http\Controllers\Company\CompanyController::class, "store"])
+        ->name("company.store");
+
+    Route::post('/uploadLogo', [\App\Http\Controllers\Company\CompanyController::class, 'updateLogo']);
+
+    Route::post('/uploadCoverImage', [\App\Http\Controllers\Company\CompanyController::class, 'updateCoverImage']);
+
+    Route::get('all-Jobs', [CompanyController::class, 'allJobs'])
         ->name('all-jobs.index');
-    Route::get('create-Job',[CompanyController::class,'addJob'])
+
+    Route::get('create-Job', [CompanyController::class, 'addJob'])
         ->name('all-jobs.create');
-    Route::post('store-Job',[CompanyController::class,'storeJob'])
+
+    Route::post('store-Job', [CompanyController::class, 'storeJob'])
         ->name('all-jobs.store');
-    Route::get('show-Job/{id}',[CompanyController::class,'showJob'])
+
+    Route::get('show-Job/{id}', [CompanyController::class, 'showJob'])
         ->name('all-jobs.show');
-    Route::get('edit-Job/{id}',[CompanyController::class,'editJob'])
+
+    Route::get('edit-Job/{id}', [CompanyController::class, 'editJob'])
         ->name('all-jobs.edit');
-    Route::put('update-Job/{id}',[CompanyController::class,'updateJob'])
+
+    Route::put('update-Job/{id}', [CompanyController::class, 'updateJob'])
         ->name('all-jobs.update');
-    Route::delete('delete-Job/{id}',[CompanyController::class,'destroyJob'])
+
+    Route::delete('delete-Job/{id}', [CompanyController::class, 'destroyJob'])
         ->name('all-jobs.destroy');
-    Route::get('jobApplications/{id}' , [JobController::class, 'getJobApplications'])
+
+    Route::get('jobApplications/{id}', [JobController::class, 'getJobApplications'])
         ->name("job.jobApplications");
-    Route::put('updateStatus/{id}' , [ApplicationController::class, 'updateStatus'])
+
+    Route::put('updateStatus/{id}', [ApplicationController::class, 'updateStatus'])
         ->name("application.updatestatus");
 });
 
@@ -121,19 +135,22 @@ Route::prefix("user")->group(function () {
         ->name("user.show-job");
 });
 
-Route::resource("profiles" , App\Http\Controllers\ProfileController::class);
+Route::resource("profiles", App\Http\Controllers\ProfileController::class);
 
-Route::resource("applications" ,ApplicationController::class);
+Route::resource("applications", ApplicationController::class);
 
-Route::resource("languages" , App\Http\Controllers\LanguageController::class);
+Route::resource("languages", App\Http\Controllers\LanguageController::class);
 
-Route::resource("educations" , App\Http\Controllers\EducationController::class);
+Route::resource("educations", App\Http\Controllers\EducationController::class);
 
 Route::get('/user/education/{id}', [App\Http\Controllers\EducationController::class, 'userEducation'])->name('user.education');
 
+Route::post('/getCitiesOfCountries', [\App\Http\Controllers\CityController::class, 'getCorrespongingCitiesForSpecificCountry']);
+
+Route::view('/adminnn', 'admin.index');
 Route::get('/admin/messages', [App\Http\Controllers\MessageController::class, 'index'])->name('messages.index');
 Route::delete('/admin/messages/{message}', [App\Http\Controllers\MessageController::class, 'destroy'])->name('messages.destroy');
 Route::put('/admin/messages/updateMessageStatus', [App\Http\Controllers\MessageController::class, 'updateStatus']);
 Route::get('/admin/password', [App\Http\Controllers\UserController::class, 'resetPassword'])->name("admin.password");
 Route::put('/admin/password', [App\Http\Controllers\UserController::class, 'updatePassword'])->name("password.update");
-Route::resource("experiences" , App\Http\Controllers\ExperienceController::class);
+Route::resource("experiences", App\Http\Controllers\ExperienceController::class);
