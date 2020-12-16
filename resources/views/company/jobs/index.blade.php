f4fa8c006510c60cd725bc0bdcc2b1ae5f994c7e
ahmedfawzy <ahmedfawzy.m1996@gmail.com>
2020-12-17 01:31:12 +0200

    Enhance company jobs view

--- resources/views/company/jobs/index.blade.php
+++ resources/views/company/jobs/index.blade.php
@@ -1,19 +1,18 @@
 @extends("company.layouts.master")
-    @section("title", "All Jobs ")
-    @section('style-sheets')
-        <link rel="stylesheet" href="{{asset('css/main.css')}}">
-        <link rel="stylesheet" href="{{asset('css/datatable.css')}}">
-    @endsection
+@section("title", "All Jobs")
+@section('style-sheets')
+<link rel="stylesheet" href="{{asset('css/main.css')}}">
+<link rel="stylesheet" href="{{asset('css/datatable.css')}}">
+@endsection
 
-    @section('content')
-        @if(session()->has('success'))
-        <div class="alert alert-success">
-            {{session()->get('success')}}
-        </div>
-        @endif
-        <a href="{{route('all-jobs.create')}}" class="btn btn-primary m-3">Add new Job</a>
-        {{-- <h1 class="text-center text-secondary ">All Jobs In {{Auth::user()->company->name}} Company</h1> --}}
-  <div class="container data-table-responsiv">
+@section('content')
+@if(session()->has('success'))
+<div class="alert alert-success">
+    {{session()->get('success')}}
+</div>
+@endif
+
+<div class="row mt-4 data-table-responsiv">
     <table id="table1" class="table table-striped table-bordered text-center table-hover">
         <thead>
             <tr>
@@ -31,7 +30,7 @@
         <tbody>
             @foreach ($jobs as $job)
             <tr>
-                <td><a href="{{route('all-jobs.show',$job->id)}}" >{{$job->title}}</a></td>
+                <td><a href="{{route('all-jobs.show',$job->id)}}">{{$job->title}}</a></td>
                 <td>{{$job->jobType->name}}</td>
                 <td>{{$job->industryCategory->name}}</td>
                 <td>{{$job->careerLevel->name}}</td>
@@ -39,33 +38,31 @@
                 <td>{{$job->city->name}}</td>
                 <td>{{$job->country->name}}</td>
                 <td>
-                    <a href="{{route('all-jobs.edit',$job->id)}}" class="btn btn-primary mr-1 ">Edit</a>
+                    <a href="{{route('all-jobs.edit',$job->id)}}" class="btn btn-warning mr-1">Edit</a>
                 </td>
                 <td>
-
-                        <form action="{{route('all-jobs.destroy',$job->id)}}" method="POST">
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-danger float-right">Delete</button>
-                        </form>
-
+                    <form action="{{route('all-jobs.destroy',$job->id)}}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger float-right">Delete</button>
+                    </form>
                 </td>
             </tr>
             @endforeach
         </tbody>
     </table>
-  </div>
-    @endsection
+</div>
+@endsection
 
-    @section('scripts')
-        <script src="{{asset('js/ajax.js')}}"></script>
-        <script src="{{asset('js/datatable.js')}}"></script>
-        <script>
-            $(document).ready(function () {
-                $('#table1').DataTable()
-                $("#nav-search").prepend($("#table1_filter"));
-                $("#table1_filter").find("input").addClass("form-control d-inline-block w-auto ml-2");
-                $("#table1_filter").find("input").attr("placeholder", "Search jobs");
-            });
-        </script>
-    @endsection
+@section('scripts')
+<script src="{{asset('js/ajax.js')}}"></script>
+<script src="{{asset('js/datatable.js')}}"></script>
+<script>
+    $(document).ready(function() {
+        $('#table1').DataTable()
+        $("#nav-search").prepend($("#table1_filter"));
+        $("#table1_filter").find("input").addClass("form-control d-inline-block w-auto ml-2");
+        $("#table1_filter").find("input").attr("placeholder", "Search jobs");
+    });
+</script>
+@endsection
