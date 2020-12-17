a4fb89a1785907f004ff9590f2e8d0d384aba4bc
ahmedfawzy <ahmedfawzy.m1996@gmail.com>
2020-12-17 15:44:24 +0200

    Fix bug in register

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -25,8 +24,6 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'role'
     ];
-
-
 
     /**
      * The attributes that should be hidden for arrays.
@@ -49,7 +46,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function jobs()
     {
-
         return $this->belongsToMany("\App\Models\Job")->withPivot("status" ,"id");
     }
 
