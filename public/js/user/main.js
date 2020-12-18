906586c83cb2848e5d3053929a69bb11ffddf3f8
Marwa Gado <mgado148@gmail.com>
2020-12-18 14:03:24 +0200

    edit application user style

--- public/js/user/main.js
+++ public/js/user/main.js
@@ -1,9 +1,8 @@
 $(function() {
-    // let minus_offset = $("#application-text").offset().top;
-
+    let offset;
     $('.application').on("click" ,function(){
-        // let offset = $(this).offset().top;
-        // $(".job").css("margin-top" , offset - 134);
+        offset = $(this).parent().offset().top;
+        const jobsContainerTopOffset = $('.jobs-container').offset().top;
         $(this).parent().addClass('active-application').siblings().removeClass('active-application');
 
         let job_id = $(this).attr("data-id");
@@ -26,43 +25,19 @@ $(function() {
                 $('.inconsediration-count').text(dataResult.inConsiderationApplicationCount);
                 $('.applied-count').text(dataResult.appliedApplicationCount);
                 $(".job").removeClass("d-none").addClass("d-block");
-                $(".job").insertAfter(".application-container[data-id = "+ job_id +"]");
+                if($(window).width() <= 768){
+                    $(".job").insertAfter(".application-container[data-id = "+ job_id +"]");
+                }
+                else{
+                    let job = $(".job");
+                    $(".job-container").append(job);
+                    job.css("margin-top" , offset - jobsContainerTopOffset);
+                }
             },
             error:function(error){
             }
+        });
     });
-    });
+
 });
 
-
-let viewed = document.getElementsByClassName("viewed");
-let applied = document.getElementsByClassName("applied");
-let other_status = document.getElementsByClassName("other-status");
-
-for(let i=0 ; i< viewed.length ; i++){
-
-    if(viewed[i].children[0].children[0].textContent == "Viewed "){
-        let applyPoint = viewed[i].children[0].children[1].children[0].classList.add("active-point");
-        let applyLine = applied[i].children[0].children[1].children[1].classList.add("active-line");
-    }
-}
-
-for(let i=0 ; i< other_status.length ; i++){
-    
-    let status_text = other_status[i].children[0].children[0];
-
-    if(status_text.textContent != " No action yet "){
-        let applyPoint = other_status[i].children[0].children[1].children[0].classList.add("active-point");
-        let applyLine = viewed[i].children[0].children[1].children[1].classList.add("active-line");
-        if(status_text.textContent == "In consideration "){
-            status_text.classList.add("in-consideration" , "font-weight-bold");
-        }
-        else if(status_text.textContent == "Not selected "){
-            status_text.classList.add("text-danger" , "font-weight-bold");
-        }
-        else if(status_text.textContent == "Selected "){
-            status_text.classList.add("text-success", "font-weight-bold");
-        }
-
-    }
-}
--- resources/views/user/show-applications.blade.php
+++ resources/views/user/show-applications.blade.php
@@ -12,9 +12,9 @@
         <span class="font-weight-bold application-text p-3" id="application-text">Applications <span>({{$jobs->count()}})</span></span>
     </div>
     <div class="row pt-3">
-        <div class="col-md-7">
+        <div class="col-sm-10 col-lg-7 jobs-container">
             @foreach($jobs as $job)
-                <div class="application-container mb-3 position-relative" data-id='{{$job->id}}'>
+                <div class="application-container mb-3" data-id='{{$job->id}}'>
                     <div class="application pt-4 pb-3 pl-3 pr-3 position-relative" data-id='{{$job->id}}'>
                         <div class="status pr-1 pl-1 mb-5 position-absolute">@if($job->status == 0) {{ "closed "}} @else {{ "open" }} @endif</div>
                         <p class="font-weight-bold job-title mt-3">{{$job->title}}</p>
@@ -25,55 +25,57 @@
                         </div>
                         <div class="p-2 pivot-status mt-4">
 
-                            <div class='job-status d-inline-block mr-n3 applied'>
-                                <div>
-                                    <p class="status-text">Applied</p>
-                                    <div class="p-0 m-0">
-                                        <div class="point d-inline-flex"></div>
-                                        <div class="line d-inline-flex mt-n1 ml-n1"></div>
-                                    </div>
-                                    <p>{{$job->pivot->created_at->diffForHumans()}}</p>
+                        <div class='job-status d-inline-block  applied p-0 m-0'>
+                                <p class="status-text p-0 m-0 @if($job->pivot->status == 'Applied') {{ 'text-danger font-weight-bolder' }} @endif ">Applied</p>
+                                <div class="p-0 m-0">
+                                    <div class="point d-inline-flex  p-0 m-0 @if($job->pivot->status == 'Applied') {{ 'active-point' }} @endif"></div>
+                                    <div class="line d-inline-flex p-0 m-0  @if($job->pivot->status != 'Applied') {{ 'active-line' }} @endif"></div>
                                 </div>
+                                <p>{{$job->pivot->created_at->diffForHumans()}}</p>
                             </div>
 
-                            <div class='job-status d-inline-block mr-n3 viewed'>
-                                <div>
-                                    <p class="status-text">@if($job->pivot->status != "Applied"){{"Viewed"}} @else {{"No action yet"}} @endif</p>
-                                    <div class="p-0 m-0">
-                                        <div class="point d-inline-flex"></div>
-                                        <div class="line d-inline-flex mt-n1 ml-n1"></div>
-                                    </div>
-                                    <p class="ml-2">@if($job->pivot->status != "Applied"){{$job->pivot->updated_at->diffForHumans()}} @else {{"."}} @endif</p>
+                            <div class='job-status d-inline-block viewed p-0 m-0'>
+                                <p class="status-text p-0 m-0  @if($job->pivot->status == 'Viewed') {{ ' text-primary font-weight-bolder' }} @endif">@if($job->pivot->status != "Applied"){{"Viewed"}} @else {{"No action"}} @endif</p>
+                                <div class="p-0 m-0">
+                                    <div class="point d-inline-flex p-0 m-0  @if($job->pivot->status == 'Viewed') {{ 'active-point' }} @elseif($job->pivot->status != 'Applied' && $job->pivot->status != 'Viewed')) {{'point-access'}} @endif"></div>
+                                    <div class="line d-inline-flex p-0 m-0  @if($job->pivot->status != 'Applied' && $job->pivot->status != 'Viewed') {{ 'active-line' }} @endif"></div>
                                 </div>
+                                <p class="ml-2 p-0 m-0">@if($job->pivot->status != "Applied"){{$job->pivot->updated_at->diffForHumans()}} @else {{"yet"}} @endif</p>
                             </div>
 
-                            <div class='job-status d-inline-block mr-n3 other-status'>
-                                <div>
-                                    <p class="status-text">@if($job->pivot->status != "Viewed" && $job->pivot->status != "Applied"){{$job->pivot->status}} @else {{"No action yet"}} @endif</p>
-                                    <div class="p-0 m-0">
-                                        <div class="point d-inline-flex"></div>
-                                    </div>
-                                    <p class="ml-2">@if($job->pivot->status != "Viewed" && $job->pivot->status != "Applied"){{$job->pivot->updated_at->diffForHumans()}} @else {{"."}} @endif</p>
+                            <div class='job-status d-inline-block other-status p-0 m-0'>
+                                <p class="status-text p-0 m-0
+                                @if($job->pivot->status == 'In consideration'){{ 'text-info font-weight-bolder'}} 
+                                @elseif($job->pivot->status == 'Selected') {{'text-success font-weight-bolder'}} 
+                                @elseif($job->pivot->status == 'Not selected') {{'text-danger font-weight-bolder'}} 
+                                @endif">
+                                @if($job->pivot->status != "Viewed" && $job->pivot->status != "Applied"){{$job->pivot->status}} @else {{"No action"}} @endif</p>
+                                <div class="p-0 m-0">
+                                    <div class="point d-inline-flex p-0 m-0 @if($job->pivot->status != 'Applied' && $job->pivot->status != 'Viewed') {{ 'active-point' }} @endif"></div>
                                 </div>
+                                <p class="ml-2">@if($job->pivot->status != "Viewed" && $job->pivot->status != "Applied"){{$job->pivot->updated_at->diffForHumans()}} @else {{"yet"}} @endif</p>
                             </div>
-
                         </div>
                     </div>
-                    <div class="position-absolute logo"><img src="{{ asset('avatar/$job->company->logo') }}" alt="" width="70px" height="70px"></div>
+                    <div class="position-absolute logo d-none d-md-block"><img src="{{ asset('avatar/$job->company->logo') }}" alt="" width="70px" height="70px"></div>
                 </div>
             @endforeach
+        </div>
+        <div class="col-sm-5">
+            <div class="job-container">
+            </div>
         </div>
 
         <div class="job d-none" id="job">
             <div class="applications_count-container pt-2 pb-2 pl-4 pr-4 mb-4">
-                    <h1 class="font-weight-bolder job_title">fgfghfhg</h1> 
-                    <span class="d-inline-block job-city">vnbbn hhbhjv</span>
+                    <h1 class="font-weight-bolder job_title"><a href=""></a></h1>
+                    <span class="d-inline-block job-city"></span>
                     <span class="country d-inline-block font-weight-bolder"></span>
-                    <p class="job-date">23/3/2020</p>
+                    <p class="job-date"></p>
                     <div class="applications_statistic"> 
                         <ul class="list-unstyled p-1 applicants-count d-inline-block">
                             <li class="d-inline-block border-right">
-                                <p class="vacancy">3</p>
+                                <p class="vacancies">3</p>
                                 <p>vacancy</p>
                             </li>
                             <li class="d-inline-block border-right">
@@ -96,7 +98,7 @@
                     </div>
             </div>
         </div>
-</div>
+    </div>
 
 
 @endsection
