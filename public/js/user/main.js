b00887d8a39394c595d36b6d7a3b894ce9af31bf
Khalid-Abed <khalidabeed24@gmail.com>
2020-12-18 22:37:03 +0200

    add background image to guest view

--- public/js/user/main.js
+++ public/js/user/main.js
@@ -1,1 +1,18 @@
-console.log('tested');
+// $('domcontentloaded').on('load',function(){
+//     console.log('success');
+// })
+
+window.addEventListener('DOMContentLoaded', function(){
+    // var path="url('/guest/category/";
+    // let images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+
+    //     var count=0;
+    //      setInterval(function(){
+    //         $('#interval').css({'background-image':path+images[count]+"')"});
+    //         count++
+    //         console.log(count);
+    //         if(count == images.length){
+    //             count = 0;
+    //         }
+    //     },3000);
+});
