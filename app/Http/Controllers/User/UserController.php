12607815edc2d420568ee396a8c57c4affbcbf37
ahmedfawzy <ahmedfawzy.m1996@gmail.com>
2020-12-20 15:23:49 +0200

    Display data in user edit view

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\User;
+
+use App\Models\IndustryCategory;
+use App\Models\Link;
+use App\Models\NumberOfEmployee;
+use App\Models\PhoneNumber;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Job;
@@ -37,7 +42,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -48,7 +53,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function show(User $user)
@@ -59,19 +64,58 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
-    public function edit(User $user)
+    public function edit()
     {
-        //
+        $user = auth()->user();
+        $linksArray = Link::select('id', 'name', 'url')->where('user_id', auth()->user()->id)->get()->toArray();
+        $phones = PhoneNumber::select('id', 'number')->where('user_id', auth()->user()->id)->get();
+        $links = [];
+        foreach ($linksArray as $oneLink) {
+            if ($oneLink['name'] == 'facebook') {
+                $links['facebook'] = $oneLink['url'];
+                $links['facebook_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'twitter') {
+                $links['twitter'] = $oneLink['url'];
+                $links['twitter_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'linkedin') {
+                $links['linkedin'] = $oneLink['url'];
+                $links['linkedin_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'behance') {
+                $links['behance'] = $oneLink['url'];
+                $links['behance_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'instagram') {
+                $links['instagram'] = $oneLink['url'];
+                $links['instagram_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'github') {
+                $links['github'] = $oneLink['url'];
+                $links['github_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'stackoverflow') {
+                $links['stackoverflow'] = $oneLink['url'];
+                $links['stackoverflow_id'] = $oneLink['id'];
+            } elseif ($oneLink['name'] == 'youtube') {
+                $links['youtube'] = $oneLink['url'];
+                $links['youtube_id'] = $oneLink['id'];
+            }
+        }
+
+        return view("user.edit", [
+            "user_id" => auth()->user()->id,
+            "user" => $user,
+            "links" => $links,
+            'phones' => $phones,
+            "industryCategories" => IndustryCategory::all(),
+            "numberOfEmployees" => NumberOfEmployee::all()
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, User $user)
@@ -79,10 +123,42 @@ class UserController extends Controller
         //
     }
 
+    public function addPhone(Request $request)
+    {
+        $request->validate([
+            'new_number' => 'required|unique:phone_numbers,number|digits_between:7,15',
+        ]);
+        PhoneNumber::create([
+            'user_id' => auth()->user()->id,
+            'number' => $request->new_number
+        ]);
+        return redirect()->back();
+    }
+
+    public function updatePhone(Request $request)
+    {
+        $request->validate([
+            'edited_number' => 'required|numeric|digits_between:7,15',
+        ]);
+        if (PhoneNumber::where('number', $request->edited_number)->where('user_id', '!=', auth()->user()->id)->count() == 0) {
+            PhoneNumber::where('id', $request->phone_id)->update(['number' => $request->edited_number]);
+            return redirect()->back();
+        } else {
+            $errors['edited_number'] = 'This number has already been taken.';
+            return redirect()->back()->withErrors($errors)->withInput();
+        }
+    }
+
+    public function deletePhone($id)
+    {
+        PhoneNumber::find($id)->delete();
+        return redirect()->back();
+    }
+
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function destroy(User $user)
@@ -97,48 +173,45 @@ class UserController extends Controller
         $descriptions = explode('.', $description);
         $related = Job::where('title', $title)->get();
         return view('user.show-job')->with('job', $job)
-                                    ->with('related', $related)
-                                    ->with('descriptions', $descriptions);
+            ->with('related', $related)
+            ->with('descriptions', $descriptions);
     }
+
     public function applyJob(Job $job)
     {
 
-       if(Auth::user()->jobs->count() > 0)
-        {
-                if (!Auth::user()->jobs->contains($job))
-                {
-                    Auth::user()->jobs()->attach($job,['status'=>'Applied']);
-                    return redirect()->back()->with(session()->flash('success','Job applied successfully'));
-                }
-                else
-                {
-                    Auth::user()->jobs()->sync($job, false);
-                    return redirect()->back()->with(session()->flash('error','This job is Already applied !'));
-                }
-        }
-        else
-        {
-            Auth::user()->jobs()->attach($job,['status'=>'Applied']);
-            return redirect()->back()->with(session()->flash('success','Congratulations Job applied successfully'));
+        if (Auth::user()->jobs->count() > 0) {
+            if (!Auth::user()->jobs->contains($job)) {
+                Auth::user()->jobs()->attach($job, ['status' => 'Applied']);
+                return redirect()->back()->with(session()->flash('success', 'Job applied successfully'));
+            } else {
+                Auth::user()->jobs()->sync($job, false);
+                return redirect()->back()->with(session()->flash('error', 'This job is Already applied !'));
+            }
+        } else {
+            Auth::user()->jobs()->attach($job, ['status' => 'Applied']);
+            return redirect()->back()->with(session()->flash('success', 'Congratulations Job applied successfully'));
         }
     }
 
-    public function showApplications(){
+    public function showApplications()
+    {
         $user = Auth::user();
         $jobs = $user->jobs;
-        return view("user.show-applications" , compact("user" , "jobs"));
+        return view("user.show-applications", compact("user", "jobs"));
     }
 
-    public function countJobApplications(Request $request){
+    public function countJobApplications(Request $request)
+    {
 
-        $job =Job::find($request["job_id"]);
+        $job = Job::find($request["job_id"]);
         $users = $job->users;
-        $usersArray = $users -> toArray();
+        $usersArray = $users->toArray();
 
         $status = array();
-        foreach($usersArray as $user){
+        foreach ($usersArray as $user) {
             $pivot_status = $user["pivot"]["status"];
-            $status[] = $pivot_status; 
+            $status[] = $pivot_status;
         }
         $notSelectedApplicationCount = count(array_keys($status, "Not selected"));
         $inConsiderationApplicationCount = count(array_keys($status, "In consideration"));
@@ -146,13 +219,13 @@ class UserController extends Controller
         $appliedApplicationCount = count($usersArray);
 
         $job_application = [
-            "viewedApplicationCount"=>$viewedApplicationCount,
-            "notSelectedApplicationCount"=>$notSelectedApplicationCount,
-            "inConsiderationApplicationCount"=>$inConsiderationApplicationCount,
-            "appliedApplicationCount"=>$appliedApplicationCount,
-            "job"=>$job,
-            "city"=>$job->city->name,
-            "country"=>$job->country->name
+            "viewedApplicationCount" => $viewedApplicationCount,
+            "notSelectedApplicationCount" => $notSelectedApplicationCount,
+            "inConsiderationApplicationCount" => $inConsiderationApplicationCount,
+            "appliedApplicationCount" => $appliedApplicationCount,
+            "job" => $job,
+            "city" => $job->city->name,
+            "country" => $job->country->name
         ];
         return json_encode($job_application);
     }
