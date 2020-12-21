672f1a03676ba2b5644eefddb29cad0f848f899f
Khalid-Abed <khalidabeed24@gmail.com>
2020-12-21 23:56:19 +0200

    create company dashboard

--- resources/views/company/dashboard.blade.php
+++ resources/views/company/dashboard.blade.php
@@ -11,12 +11,12 @@
     <div class="container mt-5">
         <div class="row">
             <!-- Jobs -->
-            <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="col-sm-6 mb-3">
                 <div class="row m-0 admin-card">
-                    <div class="col-12 row m-0 justify-content-between align-items-center admin-card-body flex-nowrap">
+                    <div class="col-12 row m-0 admin-cards justify-content-between align-items-center admin-card-body flex-nowrap">
                         <div class="admin-card-text">{{$data['jobs']}} Jobs</div>
                         <div class="card-body-icon">
-                            <i class="fab fa-houzz"></i>
+                            <i class="fas fa-briefcase"></i>‏
                         </div>
                     </div>
                     <a class="col-12 admin-card-footer" href="{{route('all-jobs.index')}}">
@@ -25,43 +25,43 @@
                 </div>
             </div>
             <!-- Employee -->
-            <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="col-sm-6 mb-3">
                 <div class="row m-0 admin-card">
-                    <div class="col-12 row m-0 justify-content-between align-items-center admin-card-body flex-nowrap">
-                        <div class="admin-card-text">{{$data['numberOfEmploies']['max']}} Employee</div>
+                    <div class="col-12 row m-0 admin-cards justify-content-between align-items-center admin-card-body flex-nowrap">
+                        <div class="admin-card-text">{{$data['numberOfEmploies']['min']}}-{{$data['numberOfEmploies']['max']}} Employee</div>
                         <div class="card-body-icon">
                             <i class="fas fa-user-tie"></i>
                         </div>
                     </div>
-                    <a class="col-12 admin-card-footer" href="{{route('all-companies.index')}}">
+                    <a class="col-12 admin-card-footer" href="#">
                         <span>View Details<i class="fa fa-angle-right ml-2"></i></span>
                     </a>
                 </div>
             </div>
             <!-- Applicants -->
-            <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="col-sm-6 mb-3">
                 <div class="row m-0 admin-card">
-                    <div class="col-12 row m-0 justify-content-between align-items-center admin-card-body flex-nowrap">
-                        <div class="admin-card-text ">{{$data['applicants']}} Applicants</div>
+                    <div class="col-12 row m-0 admin-cards justify-content-between align-items-center admin-card-body flex-nowrap">
+                        <div class="admin-card-text">{{$data['applicants']}} Applicants</div>
                         <div class="card-body-icon">
                             <i class="fas fa-blender-phone"></i>
                         </div>
                     </div>
-                    <a class="col-12 admin-card-footer" href="{{route('countries.index')}}">
+                    <a class="col-12 admin-card-footer" href="#">
                         <span>View Details<i class="fa fa-angle-right ml-2"></i></span>
                     </a>
                 </div>
             </div>
             <!-- Rate -->
-            <div class="col-xl-4 col-sm-6 mb-3">
+            <div class="col-sm-6 mb-3">
                 <div class="row m-0 admin-card">
-                    <div class="col-12 row m-0 justify-content-between align-items-center admin-card-body flex-nowrap">
+                    <div class="col-12 row m-0  admin-cards justify-content-between align-items-center admin-card-body flex-nowrap">
                         <div class="admin-card-text">{{$data['rate']}} Rate </div>
                         <div class="card-body-icon">
-                            <i class="fas fa-percentage"></i>
+                            <i class="fas fa-chart-line"></i>‏
                         </div>
                     </div>
-                    <a class="col-12 admin-card-footer" href="{{route('cities.index')}}">
+                    <a class="col-12 admin-card-footer" href="#">
                         <span>View Details<i class="fa fa-angle-right ml-2"></i></span>
                     </a>
                 </div>
